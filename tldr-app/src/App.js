c1dae3bd1e44c7614bff27a934a2c7367942b626
janesun523 <weijiejanesun@gmail.com>
2025-01-26 14:14:42 -0600

    navbar

--- tldr-app/src/App.js
+++ tldr-app/src/App.js
@@ -1,35 +1,76 @@
-// import logo from './logo.svg';
+// // import logo from './logo.svg';
+// // import './App.css';
+// //
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.js</code> and save to reload.
+// //         </p>
+// //         <a
+// //           className="App-link"
+// //           href="https://reactjs.org"
+// //           target="_blank"
+// //           rel="noopener noreferrer"
+// //         >
+// //           Learn React
+// //         </a>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+// //
+// // export default App;
+//
+// import React from 'react';
+// import { Routes, Route, Link } from 'react-router-dom';
 // import './App.css';
+// import logo from './logo.svg';
+//
+// // Import your page components (you can create these files separately)
+// import Home from './pages/Home';
+// import Preferences from './pages/Preferences';
+// import NotFound from './pages/NotFound';
+// import About from './pages/About';
 //
 // function App() {
 //   return (
 //     <div className="App">
 //       <header className="App-header">
 //         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
+//         <nav>
+//           {/* Add navigation links */}
+//           <ul>
+//             <li><Link to="/">Home</Link></li>
+//             <li><Link to="/preferences">Preferences</Link></li>
+//             <li><Link to="/about">About Us</Link></li>
+//           </ul>
+//         </nav>
 //       </header>
+//
+//       {/* Define your routes here */}
+//       <Routes>
+//         <Route path="/" element={<Home />} />
+//         <Route path="/preferences" element={<Preferences />} />
+//         <Route path="/about" element={<About />} />
+//         <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+//       </Routes>
 //     </div>
 //   );
 // }
 //
 // export default App;
 
+// src/App.js
 import React from 'react';
-import { Routes, Route, Link } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import logo from './logo.svg';
+import Navbar from './components/Navbar'; // Import the Navbar component
 
-// Import your page components (you can create these files separately)
+// Import your page components
 import Home from './pages/Home';
 import Preferences from './pages/Preferences';
 import NotFound from './pages/NotFound';
@@ -40,22 +81,15 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <nav>
-          {/* Add navigation links */}
-          <ul>
-            <li><Link to="/">Home</Link></li>
-            <li><Link to="/preferences">Preferences</Link></li>
-            <li><Link to="/about">About Us</Link></li>
-          </ul>
-        </nav>
+
+        <Navbar /> {/* Use the Navbar component */}
       </header>
 
-      {/* Define your routes here */}
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/preferences" element={<Preferences />} />
         <Route path="/about" element={<About />} />
-        <Route path="*" element={<NotFound />} /> {/* Catch-all for 404 */}
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </div>
   );
