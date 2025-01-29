89913004717577d1c6ac2aba586dfe12469f0bb7
zander-raycraft <zanderraycraft504@gmail.com>
2025-01-29 15:33:47 -0600

    updated code

--- jest.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    transform: {
-      '^.+\\.(js|jsx)$': 'babel-jest',
-    },
-    transformIgnorePatterns: [
-      '/node_modules/(?!axios)/',  // Add axios to be transformed by Babel
-    ],
-  };
-  
